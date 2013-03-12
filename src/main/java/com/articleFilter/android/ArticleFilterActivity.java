57e02868c4f5e88fff3d190dcf91d17f6b25ee9f
Jonathan WINANDY <jonathan.winandy@gmail.com>
2013-03-12 17:04:20 +0100

    ++

--- src/main/java/com/articleFilter/android/ArticleFilterActivity.java
+++
@@ -1,9 +0,0 @@
-package com.articleFilter.android;
-
-import android.app.Activity;
-import com.googlecode.androidannotations.annotations.EActivity;
-
-
-@EActivity(R.layout.main)
-public class ArticleFilterActivity extends Activity {
-}
--- src/main/java/fr/ceedela/alice/model/AddTwitterAccount.java
+++
@@ -1,7 +0,0 @@
-package fr.ceedela.alice.model;
-
-import lombok.Data;
-
-@Data public class AddTwitterAccount {
-   final String username;
-}
